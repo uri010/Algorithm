ffbaa3f9306c8856f785a471f9c45196f0a9d50b
uri010 <dbfl0461@gmail.com>
2022-01-14 17:47:16 +0900

    22.01.14-BOJ

--- C++/C++/1202_보석 도둑.cpp
+++ C++/C++/1202_보석 도둑.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 struct cmp {
-	bool operator()(pair<int, int>& a, pair <int, int>& b) {
-		if (a.second == b.second) {
-			return a.first < b.first;
+	bool operator()(pair<int, int>& a, pair<int, int>& b) {
+		if (a.first == b.first) {
+			return a.second > b.second;
 		}
-		return a.second < b.second;
+		return a.first > b.first;
 	}
 };
 
@@ -17,30 +17,40 @@ int main() {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	priority_queue <pair<int, int>, vector<pair<int, int>>, cmp> jewel;
-	vector<int> bag;
-
-	int n, k, m, v, c;
+	int n, k;
 	cin >> n >> k;
-
+	
+	priority_queue <pair<int, int>, vector<pair<int, int>>, cmp> jewel;
+	int m, v;
 	for (int i = 0; i < n; i++) {
 		cin >> m >> v;
 		jewel.push({ m,v });
 	}
+	
+	vector<int> bag;
+
+	int c;
 	for (int i = 0; i < k; i++) {
 		cin >> c;
 		bag.push_back(c);
 	}
-
+	
 	sort(bag.begin(), bag.end());
+	
+	priority_queue<int> price;
 
 	long long int sum = 0;
-	while(!jewel.empty()) {
-		int iter = lower_bound(bag.begin(), bag.end(), jewel.top().first) - bag.begin();
-		if( iter != bag.size() && )
+
+	int j = 0; 
+	for (int i = 0; i < k; i++) {
+		for (; j < n && jewel.top().first <= bag[i]; j++) {
+			price.push(jewel.top().second);
+			jewel.pop();
+		}
+		if (!price.empty()) {
+			sum += price.top();
+			price.pop();
+		}
 	}
-
 	cout << sum;
-
-
 }
