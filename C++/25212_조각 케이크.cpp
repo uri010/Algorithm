2037f0dfff986733c47ebc06a88e6e40e8fcc358
uri010 <dbfl0461@gmail.com>
2022-07-03 22:04:12 +0900

    22.07.03-BOJ

--- C++/25212_조각 케이크.cpp
+++ C++/25212_조각 케이크.cpp
@@ -1,3 +1,50 @@
-//
-// Created by 김유리 on 2022/06/27.
-//
+#include <iostream>
+using namespace std;
+
+double cakes[20];
+bool selected[20];
+int bound;
+double total;
+int n;
+int ans;
+
+void check(){
+    total =0;
+    for(int i=0; i < n; i++){
+        if( selected[i]){
+            total = total + (1/cakes[i]);
+        }
+    }
+    if( total >= 0.99 && total <= 1.01) {
+        ans++;
+    }
+}
+
+
+void DFS(int idx, int cnt){
+    if(cnt == bound ){
+        check();
+        return;
+    }
+    for(int i= idx; i < n; i++ )
+    {
+        if(selected[i] ==true) continue;
+        selected[i] = true;
+        DFS(i, cnt+1);
+        selected[i] = false;
+    }
+}
+
+int main (){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+    for(int i=0; i < n;i++){
+        cin >> cakes[i];
+    }
+    for(bound =1; bound <= n;bound++){
+        DFS(0, 0);
+    }
+    cout << ans ;
+}
