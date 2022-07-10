cd37094d1e7c21a187ce2e0b36cbdb9b0e9126e6
uri010 <dbfl0461@gmail.com>
2022-07-10 23:35:42 +0900

    22.07.10-BOJ

--- C++/16936_나3곱2.cpp
+++ C++/16936_나3곱2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <string.h>
+
 using namespace std;
 typedef unsigned long long ull;
 
@@ -9,39 +9,36 @@ bool visited[101];
 int n;
 
 void Solve(vector<ull> ans) {
-    for(int i=1; i < n; i++){
-        for(int j=0;j < n; j++){
-            if( ans.back()%3==0 && v[j] == ans.back()/3){
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (ans.back() % 3 == 0 && v[j] == ans.back() / 3) {
                 ans.push_back(v[j]);
-            }
-            else if( v[j] == ans.back()*2) {
+            } else if (v[j] == ans.back() * 2) {
                 ans.push_back(v[j]);
             }
         }
-        if( i== (n-1) ){
-            if( ans.size() == n ) {
-                for(int k=0;k < n; k++){
-                    cout << ans[k] << " " ;
+        if (i == (n - 1)) {
+            if (ans.size() == n) {
+                for (int k = 0; k < n; k++) {
+                    cout << ans[k] << " ";
                 }
-            }
-            else {
+            } else {
                 return;
             }
         }
     }
 }
 
-int main (){
-    cin.tie(NULL);ios::sync_with_stdio(false);
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
     cin >> n;
     ull a;
-    for(int i=0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cin >> a;
         v.push_back(a);
     }
-    for(int i=0;i < n; i++){
-        memset(visited, false, sizeof(visited));
-        visited[i] = true;
+    for (int i = 0; i < n; i++) {
         vector<ull> ans;
         ans.push_back(v[i]);
         Solve(ans);
