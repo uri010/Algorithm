02d584fc5c03ff0cb155b3292c9ff0cbecd3234d
uri010 <dbfl0461@gmail.com>
2022-08-04 23:15:01 +0900

    22.08.04 - 알고리즘 심화 3주차 16번

--- C++/2022 땅울림 썸머코딩 알고리즘 심화반/17780_새로운 게임.cpp
+++ C++/2022 땅울림 썸머코딩 알고리즘 심화반/17780_새로운 게임.cpp
@@ -4,23 +4,17 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#include <algorithm>
 using namespace std;
 
 int n, k;
 int a, b, c;
 int map[13][13];
-int par[13];
 vector<pair<int, int>> piece[13][13];
-queue<pair<int, pair<int, int>>> q;
+priority_queue<pair<pair<int, int>, pair<int, int>>> pq;
 
-int dj[4] = {1, -1, 0, 0};
-int di[4] = {0, 0, -1, 1};
-
-int Find(int x) {
-    if (par[x] == x) return x;
-    return par[x] = Find(par[x]);
-}
+int di[] = {0, 0, 0, -1, 1};
+int dj[] = {0, 1, -1, 0, 0};
 
 int main() {
     cin.tie(nullptr);
@@ -28,58 +22,72 @@ int main() {
     ios::sync_with_stdio(false);
 
     cin >> n >> k;
+
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             cin >> map[i][j];
         }
     }
-
-    for (int i = 1; i <= k; k++) {
+    for (int i = 1; i <= k; i++) {
         cin >> a >> b >> c;
-        piece[a][b].push_back({c, 1});
-        q.push({i, {a, b}});
+        piece[a][b].push_back({i, c});
+        pq.push({{-i, c},
+                 {a,  b}});
     }
 
     int cnt = 0;
-    int flag = 0;
     while (cnt <= 1000) {
-        for (int l = 1; l <= k; l++) {
-            int now = q.front().first;
-            int i = q.front().second.first;
-            int j = q.front().second.second;
-            q.pop();
-            int direct = piece[i][j][0].first;
+        cnt++;
+
+        while (!pq.empty()) {
+            int num = -pq.top().first.first;
+            int direct = pq.top().first.second;
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
+            pq.pop();
+
+            if (piece[i][j][0].first != num) continue;
             int ni = i + di[direct];
-            int nj = j + di[direct];
-            if (ni <= 0 || nj <= 0 || ni > n || nj > n || map[ni][nj] == 3) {
-                if (direct == 1) {
-                    direct = 2;
-                    nj = j + dj[direct];
-                } else if (direct == 2) {
-                    direct = 1;
-                    nj = j + dj[direct];
-                } else if (direct == 3) {
-                    direct = 4;
-                    ni = i + di[direct];
-                } else if (direct == 4) {
-                    direct = 3;
-                    ni = i+ di[direct];
+            int nj = j + dj[direct];
+            if (ni <= 0 || nj <= 0 || ni > n || nj > n || map[ni][nj] == 2) {
+                if (direct == 1 || direct == 3) {
+                    direct += 1;
+                } else if (direct == 2 || direct == 4) {
+                    direct -= 1;
                 }
-                if( ni <=0 || nj <=0 || ni > n || nj > n || map[ni][nj] == 3) {
-                    q.push({piece[ni][nj][0].first,
+                ni = i + di[direct];
+                nj = j + dj[direct];
+                piece[i][j][0].second = direct;
+                if (ni <= 0 || nj <= 0 || ni > n || nj > n || map[ni][nj] == 2) {
+                    continue;
                 }
-
-            }
-
-            if (map[ni][nj] == 0) {
-
             }
             if (map[ni][nj] == 1) {
-
+                reverse(piece[i][j].begin(), piece[i][j].end());
             }
+            for (auto x: piece[i][j]) {
+                piece[ni][nj].push_back({x.first, x.second});
+            }
+            if (piece[ni][nj][0].first > num) {
+                pq.push({{-piece[ni][nj][0].first, piece[ni][nj][0].second},
+                         {ni,                      nj}});
+            }
+            piece[i][j].resize(0);
+        }
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (piece[i][j].size() >= 4) {
+                    cout << cnt;
+                    return 0;
+                }
+                if (piece[i][j].size() != 0) {
+                    pq.push({{-piece[i][j][0].first,
+                                     piece[i][j][0].second},
+                             {i,     j}});
+                }
+            }
         }
     }
-
-
+    cout << -1;
 }
