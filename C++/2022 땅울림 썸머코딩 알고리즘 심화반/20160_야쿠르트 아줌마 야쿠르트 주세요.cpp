efe497b2b3713cb05917630a5d3a346af6bb14ff
uri010 <dbfl0461@gmail.com>
2022-08-10 00:11:23 +0900

    22.08.09 - 알고리즘 심화 4주차 6번

--- C++/2022 땅울림 썸머코딩 알고리즘 심화반/20160_야쿠르트 아줌마 야쿠르트 주세요.cpp
+++ C++/2022 땅울림 썸머코딩 알고리즘 심화반/20160_야쿠르트 아줌마 야쿠르트 주세요.cpp
@@ -5,33 +5,34 @@
 #include <queue>
 #include <vector>
 
-#define INF 100000000
+#define INF 99999999999999
+typedef long long ll;
 using namespace std;
 
 
-int V, E, u, v, w;
-int start;
-int dist[10002];
-vector<int> yogurt(10002);
-vector<vector<pair<int, int>>> edge;
-vector<int> route(10);
-priority_queue<pair<int, int>> pq;
+ll V, E, u, v, w;
+ll start;
+ll dist[10002];
+vector<ll> yogurt;
+vector<vector<pair<ll, ll>>> edge;
+vector<ll> route(10);
+priority_queue<pair<ll, ll>> pq;
 
-void solve(int s) {
-    priority_queue<pair<int, int>> q;
+void solve(ll s) {
+    priority_queue<pair<ll, int>> q;
     q.push({0, s});
     yogurt[s] = 0;
 
-    while (!pq.empty()) {
-        int cost = -pq.top().first;
-        int now = pq.top().second;
+    while (!q.empty()) {
+        ll cost = -q.top().first;
+        ll now = q.top().second;
         q.pop();
 
         if (yogurt[now] < cost) continue;
-        for (auto x : edge[now]) {
-            int next = x.first;
-            int next_cost = x.second+ cost;
-            if( yogurt[next] <= next_cost) continue;
+        for (auto x: edge[now]) {
+            ll next = x.first;
+            ll next_cost = x.second + cost;
+            if (yogurt[next] <= next_cost) continue;
             yogurt[next] = next_cost;
             q.push({-next_cost, next});
         }
@@ -57,45 +58,46 @@ int main() {
     for (int i = 0; i < 10; i++) {
         cin >> route[i];
     }
+    yogurt.resize(V+2);
 
     cin >> start;
     dist[start] = 0;
     pq.push({0, start});
     while (!pq.empty()) {
-        int cost = -pq.top().first;
-        int now = pq.top().second;
+        ll cost = -pq.top().first;
+        ll now = pq.top().second;
         pq.pop();
 
         if (dist[now] < cost) continue;
         for (auto x: edge[now]) {
-            int next = x.first;
-            int next_cost = x.second + cost;
+            ll next = x.first;
+            ll next_cost = x.second + cost;
             if (next_cost >= dist[next]) continue;
             dist[next] = next_cost;
             pq.push({-next_cost, next});
         }
     }
 
-    int ans = INF;
+    ll ans = INF;
     if (start == route[0]) {
         ans = start;
     }
-    int sum = 0;
+    ll sum = 0;
     for (int i = 0; i < 9; i++) {
         fill(yogurt.begin(), yogurt.end(), INF);
         solve(route[i]);
-        while (yogurt[route[i+1]] == INF) {
+        while (yogurt[route[i + 1]] == INF) {
             i++;
             if (i >= 9)
                 break;
         }
         if (i >= 9)
             break;
-        if (yogurt[route[i+1]] < INF && sum + yogurt[route[i+1]] >= dist[route[i+1]])
-            ans =  min(ans,route[i+1]);
-        if (yogurt[route[i+1]] < INF)
-            sum += yogurt[route[i+1]];
+        if (yogurt[route[i + 1]] < INF && sum + yogurt[route[i + 1]] >= dist[route[i + 1]])
+            ans = min(ans, route[i + 1]);
+        if (yogurt[route[i + 1]] < INF)
+            sum += yogurt[route[i + 1]];
     }
-    if( ans==INF) ans = -1;
+    if (ans == INF) ans = -1;
     cout << ans;
 }
