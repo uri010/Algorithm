0560cd140efd45d45d73d226b0019725d49b2915
uri010 <dbfl0461@gmail.com>
2022-08-15 22:09:21 +0900

    22.08.15 - 알고리즘 심화 5주차 4번

--- C++/2022 땅울림 썸머코딩 알고리즘 심화반/9663_N-Queen.cpp
+++ C++/2022 땅울림 썸머코딩 알고리즘 심화반/9663_N-Queen.cpp
@@ -2,73 +2,78 @@
 // Created by 김유리 on 2022/08/12.
 //
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int n;
-bool map[16][16];
-int width, length, diag1, diag2;
+int length, diag1, diag2;
 int ans;
 
 bool isValid(int i, int j) {
-    if ((width & (1 << i)) != 0) return false;
-    if ((length & (1 << j)) != 0) return false;
-    if ((diag1 & (1 << i + j)) != 0) return false;
-    while (i != 0 && j != 0) {
-        i--;
-        j--;
+    if ((diag1 & (1 << (i + j))) != 0) return false;
+    if (i - j > 0) {
+        i = i - j;
+        j = 0;
+    } else {
+        j = j - i;
+        i = 0;
     }
-    if (i == 0)
-        if ((diag2 & (1 << j + n)) != 0) return false;
-        else if ((diag2 & (1 << i)) != 0) return false;
 
+    if (i == 0) {
+        if ((diag2 & (1 << (j + n))) != 0) return false;
+    } else if (j == 0) {
+        if ((diag2 & (1 << i)) != 0) return false;
+    }
     return true;
 }
 
-void Solve(int i, int j, int cnt) {
+vector<int> AvailableNum() {
+    vector<int> tmp;
+    for (int j = 0; j < n; j++) {
+        if ((length & (1 << j)) == 0)
+            tmp.push_back(j);
+    }
+    return tmp;
+}
+
+void Solve(int i, int cnt) {
     if (cnt == n) {
         ans++;
         return;
     }
     if (i == n) return;
+    int tmp_length = length;
+    int tmp_diag1 = diag1;
+    int tmp_diag2 = diag2;
 
-    if (isValid(i, j)) {
-        int tmp_width = width;
-        int tmp_length = length;
-        int tmp_diag1 = diag1;
-        int tmp_diag2 = diag2;
-        width = width | (1 << i);
-        length = length | (1 << j);
-        diag1 = diag1 | (1 << i + j);
-        int ni = i;
-        int nj = j;
-        while (ni != 0 && nj != 0) {
-            ni--;
-            nj--;
+    vector<int> x = AvailableNum();
+    for (int j: x) {
+        if (isValid(i, j)) {
+            length = length | (1 << j);
+            diag1 = diag1 | (1 << (i + j));
+            int ni = i;
+            int nj = j;
+            if (ni - nj > 0) {
+                ni = ni - nj;
+                nj = 0;
+            } else {
+                nj = nj - ni;
+                ni = 0;
+            }
+            if (ni == 0)
+                diag2 = diag2 | (1 << (nj + n));
+            else
+                diag2 = diag2 | (1 << ni);
+
+            Solve(i + 1, cnt + 1);
+
+            length = tmp_length;
+            diag1 = tmp_diag1;
+            diag2 = tmp_diag2;
         }
-        if (ni == 0)
-            diag2 = diag2 | (1 << (nj + n));
-        else
-            diag2 = diag2 | (1 << ni);
-
-        if (j + 1 < n)
-            Solve(i, j + 1, cnt + 1);
-        else
-            Solve(i + 1, 0, cnt + 1);
-
-        width = tmp_width;
-        length = tmp_length;
-        diag1 = tmp_diag1;
-        diag2 = tmp_diag2;
-    }
-    else{
-        if (j + 1 < n)
-            Solve(i, j + 1, cnt);
-        else
-            Solve(i + 1, 0, cnt);
     }
 }
-
 
 int main() {
     cin.tie(nullptr);
@@ -76,7 +81,6 @@ int main() {
     ios::sync_with_stdio(false);
 
     cin >> n;
-    Solve(0, 0, 0);
+    Solve(0, 0);
     cout << ans;
-
 }
