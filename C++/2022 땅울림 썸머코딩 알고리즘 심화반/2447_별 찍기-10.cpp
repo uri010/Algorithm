dcdff3c95cbee633fae9fe378a5de25f2f9f7cfa
uri010 <dbfl0461@gmail.com>
2022-08-17 23:18:19 +0900

    22.08.17 - 알고리즘 심화 5주차 3번

---
+++ C++/2022 땅울림 썸머코딩 알고리즘 심화반/2447_별 찍기-10.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 김유리 on 2022/08/17.
+//
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+void Solve(int cnt) {
+    if( cnt== n){
+        return;
+    }
+}
+
+int main() {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+}
