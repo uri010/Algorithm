22c3c3e0b57af45d524d50f0f269f63167cbdb2f
uri010 <dbfl0461@gmail.com>
2022-08-23 09:28:53 +0900

    22.08.23 - 알고리즘 심화 5주차 3번

--- C++/2022 땅울림 썸머코딩 알고리즘 심화반/2447_별 찍기-10.cpp
+++ C++/2022 땅울림 썸머코딩 알고리즘 심화반/2447_별 찍기-10.cpp
@@ -7,10 +7,13 @@ using namespace std;
 
 int n;
 
-void Solve(int cnt) {
-    if( cnt== n){
-        return;
-    }
+void Solve(int i, int j, int size) {
+    if (size == 1)
+        cout << "*";
+    else if ((i / (size / 3)) % 3 == 1 && (j / (size / 3)) % 3 == 1)
+        cout << " ";
+    else
+        Solve(i, j, size / 3);
 }
 
 int main() {
@@ -19,4 +22,10 @@ int main() {
     ios::sync_with_stdio(false);
 
     cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            Solve(i, j, n);
+        }
+        cout << "\n";
+    }
 }
