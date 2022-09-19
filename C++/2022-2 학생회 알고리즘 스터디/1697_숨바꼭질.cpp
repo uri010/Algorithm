54c13ad86821ea57b4e549caf43ea87fe8e39b05
uri010 <dbfl0461@gmail.com>
2022-09-19 23:12:58 +0900

    22.09.19 - 학생회 알고리즘 스터디 2주차 5번

---
+++ C++/2022-2 학생회 알고리즘 스터디/1697_숨바꼭질.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+
+#define MAX 100001
+using namespace std;
+
+int n, k, ans;
+queue<pair<int, int>> q;
+bool visited[MAX];
+
+bool check(int i) {
+    return i >= 0 && i <= 100000 && !visited[i];
+}
+
+void BFS() {
+    while (!q.empty()) {
+        int cnt = q.front().first;
+        int i = q.front().second;
+        q.pop();
+        if (i == k) {
+            ans = cnt;
+            return;
+        }
+
+        if (check(i * 2)) {
+            visited[i * 2] = true;
+            q.push({cnt + 1, i * 2});
+        }
+        if (check(i + 1)) {
+            visited[i + 1] = true;
+            q.push({cnt + 1, i + 1});
+        }
+        if (check(i - 1)) {
+            visited[i - 1] = true;
+            q.push({cnt + 1, i - 1});
+        }
+    }
+}
+
+
+int main() {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    cin >> n >> k;
+    q.push({0, n});
+    visited[n] = true;
+    BFS();
+    cout << ans;
+}
