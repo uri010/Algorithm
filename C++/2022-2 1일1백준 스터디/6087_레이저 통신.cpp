b9c0586f72c292d3b3b37b8a10c75cf784c4cd03
yulee <dbfl0461@gmail.com>
2023-06-30 01:09:02 +0900

    6087 - 레이저 통신

--- C++/2022-2 1일1백준 스터디/6087_레이저 통신.cpp
+++ C++/2022-2 1일1백준 스터디/6087_레이저 통신.cpp
@@ -1,46 +1,60 @@
-#include <queue>
 #include <iostream>
+#include <queue>
 
 #define INF 1000000000
-
 using namespace std;
 
 int w, h;
 char map[101][101];
-int dp[101][101];
-priority_queue<pair<pair<int, int>, pair<int, int>>> pq;
-pair<int, int> start;
-pair<int, int> ended;
-int di[4] = {1, -1, 0, 0};
-int dj[4] = {0, 0, 1, -1};
+int cnt[101][101];
+queue <pair<int, int>> q;
+pair<int, int> start = {-1, -1};
+pair<int, int> fin = {-1, -1};
 
+int di[] = {0, 0, 1, -1};
+int dj[] = {1, -1, 0, 0};
+
+
+void Input() {
+    cin >> w >> h;
+    for (int i = 0; i < h; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < s.size(); j++) {
+            map[i][j] = s[j];
+            if (map[i][j] == 'C') {
+                if (start.first == -1) start = {i, j};
+                else fin = {i, j};
+            }
+            cnt[i][j] = 0;
+        }
+    }
+}
 
 void BFS() {
+    q.push({start.first, start.second});
 
-    while (!pq.empty()) {
-        int cnt = -pq.top().first.first;
-        int direct = pq.top().first.second;
-        int i = pq.top().second.first;
-        int j = pq.top().second.second;
-        pq.pop();
-
-        if( dp[i][j] < cnt ) continue;
-        dp[i][j] = cnt;
+    while (!q.empty()) {
+        int i = q.front().first;
+        int j = q.front().second;
+        q.pop();
 
         for (int k = 0; k < 4; k++) {
             int ni = i + di[k];
             int nj = j + dj[k];
-            if (ni < 0 || nj < 0 || ni >= h || nj >= w) continue;
-            if (map[ni][nj] == '*') continue;
 
-            int next_cnt = (direct == k) ? cnt : cnt + 1;
-            if (dp[ni][nj] < next_cnt) continue;
-            dp[ni][nj] = next_cnt;
-            pq.push({{-next_cnt, k},
-                     {ni,       nj}});
+            while (true) {
+                if (ni < 0 || nj < 0 || ni >= h || nj >= w) break;
+                if (map[ni][nj] == '*') break;
+                if (cnt[ni][nj] == 0) {
+                    cnt[ni][nj] = cnt[i][j] + 1;
+                    q.push({ni, nj});
+                }
+                ni += di[k];
+                nj += dj[k];
+            }
         }
     }
-    cout << dp[ended.first][ended.second];
 }
 
 int main() {
@@ -48,39 +62,7 @@ int main() {
     cout.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    cin >> w >> h;
-    string s;
-    bool flag = false;
-    for (int i = 0; i < h; i++) {
-        cin >> s;
-        for (int j = 0; j < s.size(); j++) {
-            map[i][j] = s[j];
-            if (s[j] == 'C') {
-                if (!flag) {
-                    start = {i, j};
-                    flag = true;
-                } else {
-                    ended = {i, j};
-                }
-            }
-        }
-    }
-    for (int i = 0; i < 101; i++) {
-        for (int j = 0; j < 101; j++) {
-            dp[i][j] = INF;
-        }
-    }
-
-    dp[start.first][start.second] = 0;
-    for (int k = 0; k < 4; k++) {
-        int ni = start.first + di[k];
-        int nj = start.second + dj[k];
-        if (ni < 0 || ni >= h || nj < 0 || nj >= w) continue;
-        if (map[ni][nj] == '*')continue;
-        dp[ni][nj] = 0;
-        pq.push({{0,  k},
-                 {ni, nj}});
-    }
-
+    Input();
     BFS();
+    cout << cnt[fin.first][fin.second] - 1;
 }
