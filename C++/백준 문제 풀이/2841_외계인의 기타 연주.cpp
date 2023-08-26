8933ae5afa1d0ebeaeff232671f64a8cc9991005
yulee <dbfl0461@gmail.com>
2023-08-26 14:50:02 +0900

    2841 - 외계인의 기타 연주

---
+++ C++/백준 문제 풀이/2841_외계인의 기타 연주.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 김유리 on 2023/07/07.
+//
+#include <iostream>
+using namespace std;
+
+int main () {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+
+}
