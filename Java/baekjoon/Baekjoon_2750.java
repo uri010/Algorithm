7d4ad0a859d6352a06e4c212f56e7fc13f6fd4b2
yulee <dbfl0461@gmail.com>
2023-09-15 12:04:46 +0900

    2750 - 수 정렬하기 by Bubble Sort

--- Java/baekjoon/Baekjoon_2750.java
+++ Java/baekjoon/Baekjoon_2750.java
@@ -1,21 +1,36 @@
 package baekjoon;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Baekjoon_2750 { // 백준에선 클래스 이름을 Main으로 바꿔야 함
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+    static int[] arr;
+
+    static void swap(int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr);
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(j, j + 1);
+                }
+            }
+        }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             System.out.println(arr[i]);
         }
     }
